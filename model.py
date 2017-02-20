f1dd72f27979bfa358e185962e1b60d771409e87
Stephen Butler <wefell@gmail.com>
2017-02-19 17:24:50 -0700

    Add files via upload

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
@@ -18,7 +19,7 @@ with open('./data/driving_log.csv') as csvfile:
         lines.append(line)
 
 # split data into training and validation sets
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+train_samples, validation_samples = train_test_split(lines, test_size=0.3)
 
 # generator function yielding image arrays and steerig angles
 def generator(lines, batch_size=32):
@@ -75,10 +76,15 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(30,30))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -87,12 +93,19 @@ model.add(Dense(1))
 
 # compile and train the model using the generator function
 model.compile(optimizer='adam', loss='mse')
+
+checkpoint = ModelCheckpoint('model.{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', verbose=1,
+                             save_best_only=True, save_weights_only=False)
+
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10,
+                               verbose=1)
+
 model.fit_generator(train_generator, samples_per_epoch=13824,
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples), nb_epoch=15)
+                    nb_val_samples=len(validation_samples), nb_epoch=200, callbacks=[checkpoint])
 
 # save model
-model.save('model.h5')
+##model.save('model.h5')
 
 # fix for known keras error
 from keras import backend as K
